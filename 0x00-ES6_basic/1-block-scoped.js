18b5b5419dedf0b2d9840352cd0157bbf4b8677f
root <wrefordmessi@gmail.com>
2023-03-20 12:08:35 -0700

    comment on eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,6 +2,7 @@ export default function taskBlock(trueOrFalse) {
 	const task = false;
 	const task2 = true;
 
+	/* eslint-disable no-unused-vars */
 	if (trueOrFalse) {
 		const task = true;
 		const task2 = false;
