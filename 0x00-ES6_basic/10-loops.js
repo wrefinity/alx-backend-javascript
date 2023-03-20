f5877d8721d31534b9625fe04e805619e273a0f8
root <wrefordmessi@gmail.com>
2023-03-20 11:55:48 -0700

    appening to array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(arr, appendString) {
   const appended = [];
-  for (const value of arr)
+  for (const value of arr){
     appended.push(appendString + value);
+  }
   return appended;
 }
