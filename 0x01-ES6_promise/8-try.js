60e6cf2616209cae41cc76d1c948183d6c37dc4f
root <wrefordmessi@gmail.com>
2023-03-22 02:50:46 -0700

    conditional check for indivisibility by zero with error throwing

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0)
+  if (denominator === 0){
     throw Error('cannot divide by 0');
-  else
+  }else{
     return numerator / denominator;
+  }
 }
