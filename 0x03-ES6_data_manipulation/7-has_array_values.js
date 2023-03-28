960d9e01260ceee2ae3af55bce99e4402d843760
root <wrefordmessi@gmail.com>
2023-03-28 09:00:19 -0700

    checking values in an array

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,9 @@
 /**
- * Checks that a set contains each element in an array.
+ * Checks if a set contains each element in an array.
  * @param {Set} set - The collection of unique items.
  * @param {*} array - The array of items.
  * @returns {Boolean}
  */
 export default function hasValuesFromArray(set, array) {
-  return array.every((el) => set.has(e1));
+  return array.every((value) => set.has(value));
 }
