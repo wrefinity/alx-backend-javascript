1f1b12b194d7a86fbefaf761afdd9afbbe4f8ce0
root <wrefordmessi@gmail.com>
2023-03-28 09:09:28 -0700

    checking if the set contain some values and cleaning the set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,23 +1,9 @@
-/**
- * Join some strings using  a dash after stripping the leading sub string.
- * @param {Set<String>} set - A collection of strings.
- * @param {String} startString - The string to strip from the beginning
- * of each item in the set.
- * @returns {String}
- */
-export default function cleanSet(set, startString) {
-  const all_str = [];
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+export default const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
-  for (const val of set.values()) {
-    if (typeof val === 'string' && val.startsWith(startString)) {
-      const valSubStr = val.substring(startString.length);
-
-      if (valSubStr && valSubStr !== val) {
-        all_str.push(valSubStr);
-      }
-    }
-  }
-  return all_str.join('-');
-}
+  return [...set]
+    .filter((p) => (p !== undefined ? p.startsWith(startString) : ''))
+    .map((p) => (p !== undefined ? p.slice(startString.length) : ''))
+    .join('-');
+};
