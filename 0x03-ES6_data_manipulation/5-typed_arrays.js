e50142e25ea6ada18a73a4f328bbd48b4359489f
root <wrefordmessi@gmail.com>
2023-03-28 02:01:36 -0700

    buffer creation and addition

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+/**
+ * buffer creation based on a given set of input
+ * @param {Number} length - The length of the buffer.
+ * @param {Number} position - The position to modify.
+ * @param {Number} value - The value to be stored in the position.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {DataView}
+ */
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
+}
